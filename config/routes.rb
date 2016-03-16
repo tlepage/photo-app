821ad8ae034cde8f85621b4e4e70f736b7b148bc
Tom LePage <thomaslepage@live.com>
2016-03-16 15:15:30 +0000

    Fixed routes and added links

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'images/create', to: 'images#create'
+  get 'images', to: 'images#index
+  '
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
